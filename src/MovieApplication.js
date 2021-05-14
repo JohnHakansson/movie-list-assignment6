d39c5246902fbb7b1f547a3b7794adcb2cd9be16
John <john.hs.hakansson@outlook.com>
2021-05-14 10:29:41 +0200

    Tagit bort kommenterade saker

--- src/MovieApplication.js
+++ src/MovieApplication.js
@@ -1,7 +1,6 @@
 import React, { useState } from "react";
 import AddMovieForm from "./AddMovieForm";
 import Movies from "./Movies";
-import Movie from "./Movie";
 import "./MovieApplication.css";
 import OrderByAlphaBtn from "./OrderByAlphaBtn";
 import OrderByGradeBtn from "./OrderByGradeBtn";
@@ -19,37 +18,13 @@ export default function MovieApplication() {
     setMovies(movies.filter((movie) => movie.id !== id));
   };
 
-  // const orderByAlphabet = () => {
-  //   movies.sort((a, b) => a.grade.localeCompare(b.grade));
-  //   setMovies(movies.filter((movie) => movie.id !== "1"));
-  // };
-
-  // const orderByGrade = () => {
-  //   movies.sort((a, b) => b.grade.localeCompare(a.grade));
-  //   setMovies(movies.filter((movie) => movie.id !== "1"));
-  // };
-
   return (
     <div className="container">
       <AddMovieForm movies={movies} setMovies={setMovies} />
-      {/* <ul>
-        {movies.map((movie) => (
-          <Movie key={movie.id} movie={movie} deleteMovie={deleteMovie} />
-        ))}
-      </ul> */}
+
       <Movies movies={movies} deleteMovie={deleteMovie} />
-      {/* <button
-        type="button"
-        className="btn btn-primary"
-        onClick={orderByAlphabet}
-      >
-        Alfabetisk Ordning
-      </button> */}
 
       <OrderByAlphaBtn movies={movies} setMovies={setMovies} />
-      {/* <button type="button" className="btn btn-primary" onClick={orderByGrade}>
-        Betygsordning
-      </button> */}
       <OrderByGradeBtn movies={movies} setMovies={setMovies} />
     </div>
   );
